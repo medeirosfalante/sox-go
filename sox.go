e907d14003949a683fe66bb9306ebc36d9392692
Rafael Medeiros <rafaelgurudesign@gmail.com>
2020-10-14 19:48:42 -0300

    trim file

--- sox.go
+++ sox.go
@@ -1,1 +1,37 @@
 package sox
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+// Sox parent group for all functions in sox-go
+type Sox struct {
+}
+
+// NewSox Create a new instance for sox
+func NewSox() Sox {
+	return Sox{}
+}
+
+//File - return output file define
+type File struct {
+	FilePath string
+}
+
+//Trim - is func for crop the file sound -
+func (s Sox) Trim(file string, outputFile string, start float32, duration float32) (*File, error) {
+	cmd := exec.Command("sox", file, outputFile, "trim", fmt.Sprintf("%f", start), fmt.Sprintf("%f", duration))
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("in all caps: %q\n", out.String())
+	return &File{
+		FilePath: out.String(),
+	}, nil
+}
--- sox_test.go
+++ sox_test.go
@@ -1,7 +1,24 @@
 package sox_test
 
-import "testing"
+import (
+	"testing"
+
+	sox "github.com/rafaeltokyo/sox-go"
+)
 
 func TestTrimAudio(t *testing.T) {
-
+	soxClient := sox.NewSox()
+	filepath := "./audios/sample.wav"
+	filepathOutput := "./result/output.wav"
+	start := float32(2)
+	duration := float32(10)
+	file, err := soxClient.Trim(filepath, filepathOutput, start, duration)
+	if err != nil {
+		t.Errorf("err : %s", err)
+		return
+	}
+	if file == nil {
+		t.Error("file is null")
+		return
+	}
 }
